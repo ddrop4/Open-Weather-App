246bf006aa9b6e33b466a0d5087782edbdc31c02
Chernyshev Anton <an.chernyshev@corp.mail.ru>
2020-10-05 14:48:49 +0300

    alert controller added

--- OpenWeatherApp/ViewController.swift
+++ OpenWeatherApp/ViewController.swift
@@ -20,6 +20,23 @@ class ViewController: UIViewController {
     let customView = UILabel(frame: .zero)
     let loader = UIActivityIndicatorView(style: .gray)
     var weatherArray: [Int] = []
+    
+    private lazy var alertController: UIAlertController = {
+        let errorAlert = UIAlertController(title: "Ошибка", message: "Такого города не существует", preferredStyle: .alert)
+        let error = UIAlertAction(title: "ОК", style: .default, handler: nil)
+        let alert = UIAlertController(title: "Добавьте свой город", message: "Название города должно быть написано используя только латинские буквы", preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Сохранить", style: .default) { action in
+            guard let textField = alert.textFields?.first else { return }
+        }
+
+        alert.addAction(saveAction)
+        alert.addAction(UIAlertAction(title: "Отмена", style: .cancel, handler: nil))
+        alert.addTextField(configurationHandler: nil)
+        
+        errorAlert.addAction(error)
+        
+        return alert
+    }()
 
     // MARK: - LifeCycle
     
